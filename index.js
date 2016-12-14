3d122422f4c2fbdf30cc943884c413e03027a20a
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2016-12-14 14:24:11 +0500

    fix code style

--- index.js
+++ index.js
@@ -6,44 +6,44 @@ const KEY_PREFIX = '#EXT-X-'
 
 module.exports = m3u
 
-function m3u(playlist) {
+function m3u (playlist) {
   const lines = playlist.toString().split('\n')
 
   if (!lines.length || !lines[0].startsWith('#EXTM3U')) {
     throw new Error('Invalid m3u playlist')
   }
 
-  return lines.slice(1).map(line => line.startsWith("#") ? transform(line.trim()) : line.trim()).filter(Boolean)
+  return lines.slice(1).map(line => line.startsWith('#') ? transform(line.trim()) : line.trim()).filter(Boolean)
 }
 
-function transform(line) {
+function transform (line) {
   const splitted = split(line)
 
-  return {[normal_key(splitted[0])]: (splitted.length == 1) ? void 0 : parse_params(splitted[1])}
+  return {[normalKey(splitted[0])]: (splitted.length === 1) ? void 0 : parseParams(splitted[1])}
 }
 
-function parse_params(line) {
+function parseParams (line) {
   const pairs = line.split(NON_QUOTED_COMMA).map(e => e.trim()).filter(Boolean)
   const attrs = {}
 
   for (const kv of pairs) {
-    const kv_list = kv.split(KV_SPLITTER)
+    const kvList = kv.split(KV_SPLITTER)
 
-    if (pairs.length == 1 && kv_list.length == 1) {
-      return kv_list[0]
+    if (pairs.length === 1 && kvList.length === 1) {
+      return kvList[0]
     }
 
-    attrs[ kv_list[0].trim() ] = kv_list.length > 1 ? kv_list[1].trim() : void 0
+    attrs[ kvList[0].trim() ] = kvList.length > 1 ? kvList[1].trim() : void 0
   }
 
   return attrs
 }
 
-function normal_key(key) {
+function normalKey (key) {
   return key.startsWith(KEY_PREFIX) ? key.slice(KEY_PREFIX.length) : key.startsWith('#') ? key.slice(1) : key
 }
 
-function split(line) {
-  const pos = line.indexOf(':')  
-  return pos > 0 ? [ line.slice(0, pos), line.slice(pos+1) ] : [line]
+function split (line) {
+  const pos = line.indexOf(':')
+  return pos > 0 ? [ line.slice(0, pos), line.slice(pos + 1) ] : [line]
 }
--- test.js
+++ test.js
@@ -10,36 +10,36 @@ const template = [
   {STATUS: void 0},
   {'ID3-EQUIV-TDTG': '2015-01-01T01:21:00'},
   { MEDIA: {
-      TYPE: 'VIDEO',
-      'GROUP-ID': 'chunked',
-      NAME: 'Source',
-      AUTOSELECT: 'YES',
-      DEFAULT: 'YES'
-    }
+    TYPE: 'VIDEO',
+    'GROUP-ID': 'chunked',
+    NAME: 'Source',
+    AUTOSELECT: 'YES',
+    DEFAULT: 'YES'
+  }
   },
   { 'STREAM-INF': {
-      'PROGRAM-ID': '1',
-      BANDWIDTH: '3454382',
-      RESOLUTION: '1280x720',
-      VIDEO: 'chunked'
-    }
+    'PROGRAM-ID': '1',
+    BANDWIDTH: '3454382',
+    RESOLUTION: '1280x720',
+    VIDEO: 'chunked'
+  }
   },
   'http://1.example.com/index.m3u8',
   { MEDIA: {
-      TYPE: 'VIDEO',
-      'GROUP-ID': 'high',
-      NAME: 'High',
-      AUTOSELECT: 'YES',
-      DEFAULT: 'YES'
-    }
+    TYPE: 'VIDEO',
+    'GROUP-ID': 'high',
+    NAME: 'High',
+    AUTOSELECT: 'YES',
+    DEFAULT: 'YES'
+  }
   },
   { 'STREAM-INF': {
-      'PROGRAM-ID': '1',
-      BANDWIDTH: '1760000',
-      RESOLUTION: '1280x720',
-      VIDEO: 'high',
-      CODECS: 'avc1.42C01E,mp4a.40.2'
-    }
+    'PROGRAM-ID': '1',
+    BANDWIDTH: '1760000',
+    RESOLUTION: '1280x720',
+    VIDEO: 'high',
+    CODECS: 'avc1.42C01E,mp4a.40.2'
+  }
   },
   'http://2.example.com/index.m3u8',
   { 'PLAYLIST-TYPE': 'VOD' },
