4ffddea1600da2ae6099b7c12a9dda21b7fbf1f4
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2016-12-14 14:09:46 +0500

    use @avajs for tests

--- test.js
+++ test.js
@@ -1,26 +1,27 @@
 'use strict'
 
-const m3u8 = require('./')
-const fs = require('fs')
+import m3u8 from './'
+import * as fs from 'fs'
+import test from 'ava'
+
 const playlist = fs.readFileSync('test.m3u8', 'utf8')
-const test = require('tape')
 
 const template = [
   {STATUS: void 0},
   {'ID3-EQUIV-TDTG': '2015-01-01T01:21:00'},
-  { MEDIA: { 
+  { MEDIA: {
       TYPE: 'VIDEO',
       'GROUP-ID': 'chunked',
       NAME: 'Source',
       AUTOSELECT: 'YES',
-      DEFAULT: 'YES' 
+      DEFAULT: 'YES'
     }
   },
   { 'STREAM-INF': {
       'PROGRAM-ID': '1',
       BANDWIDTH: '3454382',
       RESOLUTION: '1280x720',
-      VIDEO: 'chunked' 
+      VIDEO: 'chunked'
     }
   },
   'http://1.example.com/index.m3u8',
@@ -45,9 +46,6 @@ const template = [
   { 'EXTINF': '10' }
 ]
 
-test('m3u8', function (t) {
-  const parsed = m3u8(playlist)
-
-  t.deepEqual(parsed, template)
-  t.end()
+test('m3u8', t => {
+  t.deepEqual(m3u8(playlist), template)
 })
